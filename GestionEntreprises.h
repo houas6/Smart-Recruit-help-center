ba46f5ca4021f45da198a01dc14e8b77299f71bf
FtouhiS <80210081+FtouhiS@users.noreply.github.com>
2021-10-26 15:21:24 +0100

    Update and rename connection.h to GestionEntreprises.h

---
+++ GestionEntreprises.h
@@ -0,0 +1,14 @@
+#ifndef GestionEntreprises_H
+#define GestionEntreprises_H
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+
+class entreprise
+{
+public:
+    Entreprise();
+   ;
+};
+
+#endif // CO_H
