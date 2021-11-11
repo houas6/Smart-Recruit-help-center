1503778f58a27ebbf7b0066d80bdff44facc73cf
houas6 <medaziz.houas@esprit.tn>
2021-11-11 14:18:45 +0100

    connection.cpp update changement bdd

--- connection.cpp
+++ connection.cpp
@@ -8,7 +8,7 @@ Connection::Connection()
 bool Connection::createConnection()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-db.setDatabaseName("recruit");
+db.setDatabaseName("test-bd");
 db.setUserName("system");//inserer nom de l'utilisateur
 db.setPassword("1234");//inserer mot de passe de cet utilisateur
 
